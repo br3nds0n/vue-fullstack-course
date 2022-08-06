95c8e16ab84aad273d158f0318c62f7f47d4ecd5
Brendson Victor <brendson.dev@gmail.com>
2022-08-06 17:06:57 -0300

    feat: starting api consumption

--- src/pages/categories/CategoriesView.vue
+++ src/pages/categories/CategoriesView.vue
@@ -1,15 +1,33 @@
 <template>
   <div>
-    <h1>Categories</h1>
+    <div class="categories">
+      <h1>Categories</h1>
+      <div class="list">
+        {{categories}}
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
-
+  data () {
+    return {
+      categories: {}
+    }
+  },
+  created () {
+    this.getCategories()
+  },
+  methods: {
+    getCategories () {
+      axios.get('http://localhost:5000/api/categories').then(response => {
+        this.categories = response.data
+      })
+    }
+  }
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
