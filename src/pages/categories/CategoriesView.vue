9fe83c098d7d80cb0ec0e77204bf01a5ecee5b49
Brendson Victor <brendson.dev@gmail.com>
2022-08-07 13:30:26 -0300

    style: customizing category list

--- src/pages/categories/CategoriesView.vue
+++ src/pages/categories/CategoriesView.vue
@@ -2,8 +2,9 @@
   <div>
     <div class="categories">
       <h1>Categories</h1>
-      <div class="list">
-        {{categories}}
+      <div class="list" v-for="category in categories" :key="category.id">
+        <div class="dados">{{category.id}}</div>
+        <div class="dados">{{category.name}}</div>
       </div>
     </div>
   </div>
@@ -30,4 +31,23 @@ export default {
 }
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+
+.categories{
+  align-items: center;
+  text-align: center;
+  .list{
+    width: 50%;
+    margin-left: 22.5%;
+    align-items: center;
+    padding: 1%;
+    display: flex;
+    justify-content: center;
+    border: 0.1px solid grey;
+    .dados{
+      padding: 0.7%;
+    }
+  }
+}
+
+</style>
