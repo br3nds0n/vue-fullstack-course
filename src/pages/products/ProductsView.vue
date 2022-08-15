03ec0a8370a68805bbf6c6913d22d2ae48def6e9
Brendson Victor <brendson.silva@equipeblue.com.br>
2022-08-15 10:39:09 -0300

    feat: store of products

--- src/pages/products/ProductsView.vue
+++ src/pages/products/ProductsView.vue
@@ -12,7 +12,7 @@
           </li>
         </ul>
       </div>
-      <div v-for="dado in dados" :key="dado.id">
+      <div v-for="dado in products.products" :key="dado.id">
         <ul>
           <li>
             <div class="dados" id="id">{{ dado.id }}</div>
@@ -28,17 +28,23 @@
 </template>
 
 <script>
-import dados from '../../data/data.json'
+import { mapState, mapActions } from 'vuex'
+
 export default {
   data () {
-    return {
-      dados: dados
-    }
+    return {}
   },
   methods: {
+    ...mapActions(['getProducts']),
     valor (valor) {
       return 'R$ ' + valor.toFixed(2)
     }
+  },
+  computed: {
+    ...mapState(['products'])
+  },
+  created () {
+    this.getProducts()
   }
 }
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,19 +1,38 @@
-import { createStore } from 'vuex'
+import Vuex from 'vuex'
+import Axios from 'axios'
 
-export default createStore({
+const categories = {
+  state: {
+    categories: []
+  }
+}
+
+const products = {
   state: {
     products: []
   },
+
   mutations: {
-    addProducts (state, payload) {
-      state.products.push(payload)
+    getProductsM (state, payload) {
+      state.products = payload
     }
   },
+
   actions: {
-    addProdutos ({ commit }, payload) {
-      commit('addProducts', payload)
+    getProducts ({ commit }) {
+      Axios.get('http://localhost:5000/api/produtos').then(res => {
+        console.log(res.data)
+        commit('getProductsM', res.data)
+      })
     }
-  },
+  }
+}
+
+const store = new Vuex.Store({
   modules: {
+    categories,
+    products
   }
 })
+
+export default store
